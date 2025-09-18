c6ab11c64ac8180538b43bb4163def8835207886
Hanae-Iken <hanaeiken572@gmail.com>
2025-09-18 12:42:47 +0100

    feat:feedback features

---
+++ book-network/src/main/java/com/hanae/book/feedback/FeedbackMapper.java
@@ -0,0 +1,32 @@
+package com.hanae.book.feedback;
+
+import com.hanae.book.book.Book;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
+public class FeedbackMapper {
+
+    public Feedback toFeedback(FeedbackRequest request) {
+        return Feedback.builder()
+                .note(request.note())
+                .comment(request.comment())
+                .book(Book.builder()
+                        .id(request.bookId())
+                        .archived(false) // Not required, has no impact, we did it just to satisfy lombok
+                        .shareable(false) // the same here
+                        .build()
+                )
+                .build();
+
+    }
+
+    public FeedbackResponse toFeedbackResponse(Feedback feedback, Integer id) {
+        return FeedbackResponse.builder()
+                .note(feedback.getNote())
+                .comment(feedback.getComment())
+                .ownFeedback(Objects.equals(feedback.getCreatedBy(), id))
+                .build();
+    }
+}
